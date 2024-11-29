8943305414c3a959c8ae8b9417b7b0569fc8360f
manel <manelbouattour114@gmail.com>
2024-11-29 10:37:59 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mon application Laravel</title>
+
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+<link href="{{ secure_asset('frontend/assets/index-CPXlk9qb.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="root"></div>
+<script src="{{ secure_asset('frontend/assets/index-zzuoi4yD.js')}}"></script>
+</body>
+</html>
